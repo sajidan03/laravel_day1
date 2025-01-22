993a723953e10ac998a3bf93d6af64203ed49407
MyPC PRO <MyPC PRO@MYPCPRO>
2025-01-22 08:44:28 +0700

    Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Member;
+use Faker\Guesser\Name;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User as AuthUser;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +17,31 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'id' => 1,
+            'name' => 'Muhammad Sajidan',
+            'email' => 'rifansyahsajidan03@gmail.com',
+            'password' => bcrypt('admin'),
+        ]);
+        User::create([
+            'id' => 2,
+            'name' => 'Muhammad Sajidan',
+            'email' => 'akunjidan03@gmail.com',
+            'password' => bcrypt('admin'),
+        ]);
+        Member::create([
+            'id' => 1,
+            'name' => "Muhammad Sajidan",
+            'no_hp' => "081563854329",
+            'address' => "Sukamanah",
+            'users_id' => 1,
+        ]);
+        Member::create([
+            'id' => 2,
+            'name' => "Muhammad Sajidan",
+            'no_hp' => "080330005076",
+            'address' => "Sukarame",
+            'users_id' => 2,
         ]);
     }
 }
